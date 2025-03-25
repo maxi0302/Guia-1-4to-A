5d8fd50917bed193e1d3ce6a8c5d90a1bccc0987
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-03-25 11:49:58 -0300

    Variables mostradas en consola

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -12,7 +12,23 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        cantidadAlumnos = 24;
+        magnitudTerremoto = 9.3f;
+<<<<<<< HEAD
+        nombreAlumno = "Matsimo";
+=======
+        nombreAlumno = "Jeronimo";
+>>>>>>> c93d6cba8d1f533f280c41f2a0b483c25e88a314
+        compuHabilitada = false;
+
+        Debug.Log(cantidadAlumnos);
+        Debug.Log(magnitudTerremoto);
+        Debug.Log(nombreAlumno);
+        Debug.Log(compuHabilitada);
+<<<<<<< HEAD
+=======
+
+>>>>>>> c93d6cba8d1f533f280c41f2a0b483c25e88a314
     }
 
     // Update is called once per frame
