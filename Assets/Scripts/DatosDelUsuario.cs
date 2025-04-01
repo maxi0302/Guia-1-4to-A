95707e3f8f097df19bb893ed1593fd813a8d0bb9
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-01 11:33:45 -0300

    correccion

--- Assets/Scripts/DatosDelUsuario.cs
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -6,7 +6,7 @@ public class DatosDelUsuario : MonoBehaviour
 {
     public string nombreUsuario;
     [SerializeField] int edadUsuario;
-<<<<<<< HEAD
+
     [SerializeField] float alturaUsuario;
     [SerializeField] bool esDonante;
 
@@ -15,14 +15,6 @@ public class DatosDelUsuario : MonoBehaviour
     {
         Debug.Log("Mi nombre es " + nombreUsuario);
         Debug.Log("Tengo " + edadUsuario + " años.");
-=======
-    public float altura;
-    public bool donante;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Mi nombre es " + nombreUsuario + " tengo " + edadUsuario + " años, mido " + altura + " tambien " + donante + " donante ");
->>>>>>> 0e2f0060195d6ea9d072905436753070b91c56be
     }
 
     // Update is called once per frame
